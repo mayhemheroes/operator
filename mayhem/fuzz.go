c896319ce74c550a83256847f5eefcf59a34fa44
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-31 01:34:07 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,46 @@
+package fuzz
+
+import "strconv"
+import "github.com/tigera/operator/pkg/render/common/podaffinity"
+import "github.com/tigera/operator/pkg/dns"
+import "github.com/tigera/operator/pkg/controller/status"
+import "github.com/tigera/operator/pkg/url"
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) > 1 {
+        num, _ = strconv.Atoi(string(bytes[0]))
+
+        switch num {
+    
+        case 0:
+            content := string(bytes)
+            podaffinity.NewPodAntiAffinity(content, "mayhem")
+            return 0
+
+        case 1:
+            content := string(bytes)
+            dns.GetClusterDomain(content)
+            return 0
+
+        case 2:
+            content := string(bytes)
+            var test status.MockStatus
+            test.RemoveCertificateSigningRequests(content)
+            return 0
+
+        default:
+            content := string(bytes)
+            url.ParseEndpoint(content)
+            return 0
+
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
